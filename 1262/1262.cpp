10432aba7ae0769a8e6ccb6db89a65d882b87513
Dowon Lee <ldwlee03@gmail.com>
2024-04-19 11:21:33 +0900

    Update 1262.cpp

--- 1262/1262.cpp
+++ 1262/1262.cpp
@@ -1,35 +1,54 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
+void Solution1()
+{
+    int M[1000] = { 0 };
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> M[i];
+    }
+    sort(M, M + N);
+    int pr = M[0];
+    cout << M[0] << " ";
+    for (int i = 1; i < N; i++)
+    {
+        if (pr != M[i])
+        {
+            cout << M[i] << " ";
+            pr = M[i];
+        }
+    }
+    cout << "\n";
+}
+void Solution2()
+{
+    int M[1001] = { 0 };
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        int a;
+        cin >> a;
+        M[a]++;
+    }
+    for (int i = 0; i < 1001; i++)
+    {
+        if (M[i] > 0)
+            cout << i << " ";
+    }
+    cout << "\n";
+}
+
 int main(void)
 {
     int T;
-    while(T--)
+    cin >> T;
+    while (T--)
     {
-        int M[1000];
-        int N;
-        cin >> N;
-        for(int i = 0; i < N; i++)
-        {
-            cin >> M[i];
-        }
-
-        for(int i = 0; i < N; i++)
-        {
-            for(int j = 0; j < N; j++)
-            {
-                if((M[i] == M[j]) && (i != j))
-                {
-                    M[j] = 0;
-                }
-            }
-        }
-
-        for(int i = 0; i < N; i++)
-        {
-            if(M[i] != 0)
-                cout << M[i] << " ";
-        }
-        cout << "\n";
+        //Solution1();
+        Solution2();
     }
     return 0;
 }
